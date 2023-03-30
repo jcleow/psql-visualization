9243c2e3bd14adbfd4767d350e63522ded64a194
jcleow <>
2023-03-30 14:02:06 +0800

    fix(eslint-issues): fix eslint issues

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+/* eslint-disable indent */
+/* eslint-disable @typescript-eslint/indent */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+    reactStrictMode: true
 }
 
 module.exports = nextConfig
