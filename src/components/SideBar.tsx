94b0925cefcf556f9a91715cb9506d54cb9d0d14
jcleow <>
2023-03-30 14:05:08 +0800

    fix(eslint-issues): fix more build issues

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import { type ReactNode } from 'react'
 import Box from '@mui/material/Box'
 import Drawer from '@mui/material/Drawer'
 import CssBaseline from '@mui/material/CssBaseline'
